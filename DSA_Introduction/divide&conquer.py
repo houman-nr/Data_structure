1f05643b07c0afa2e3503acba80608a4ee5934e9
houman-nr <houmannoraste80@gmail.com>
2023-01-31 16:50:03 +0330

    merging part of merge sort is added

--- DSA_Introduction/divide&conquer.py
+++ DSA_Introduction/divide&conquer.py
@@ -2,14 +2,27 @@
 def mergeSort(array, start, end):
     if start < end:
         return
-    mergeSort(array, start, int(end + start/2))
-    mergeSort(array, int(end + start/2)+1, end)
+    middle = int(end + start/2)
+    mergeSort(array, start, middle)
+    mergeSort(array, middle+1, end)
+    merge(array, start, middle, end)
 
 def merge(array, start, middle, end):
-    pass
+    leftHalf = array[start:middle]
+    rightHalf = array[middle+1:end]
+    print("left half:")
+    print("left half:")
+    j ,k = 0
+    for i in range(array):
+        if leftHalf[j] > rightHalf[k]:
+            array[i] = leftHalf[j]
+            j = j + 1
+        else:
+            array[i] = rightHalf[k]
+            k = k + 1
 #this is a divide and conquer approach to sorting problem
 
 if __name__ == "__main__":
     first_array = [5, 3, 9, 2, 7]
-    mergeSort(first_array, 0, 5)
+    mergeSort(first_array, 0, 4)
     print(first_array)
