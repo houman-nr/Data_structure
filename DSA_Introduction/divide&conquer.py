5adc5ab65bbeb02853de542c0681177563ee3ce5
houman-nr <houmannoraste80@gmail.com>
2023-02-01 15:53:07 +0330

    merge sort is finished

--- DSA_Introduction/divide&conquer.py
+++ DSA_Introduction/divide&conquer.py
@@ -1,28 +1,49 @@
 #this is a divide and conquer approach to sorting problem
 def mergeSort(array, start, end):
-    if start < end:
+    if start == end:
         return
-    middle = int(end + start/2)
+    middle = int((end + start)/2)
     mergeSort(array, start, middle)
     mergeSort(array, middle+1, end)
-    merge(array, start, middle, end)
+    merge(array, start, middle+1, end)
 
 def merge(array, start, middle, end):
+    
     leftHalf = array[start:middle]
-    rightHalf = array[middle+1:end]
-    print("left half:")
-    print("left half:")
-    j ,k = 0
-    for i in range(array):
-        if leftHalf[j] > rightHalf[k]:
-            array[i] = leftHalf[j]
-            j = j + 1
+    rightHalf = array[middle:end+1]
+    i = start
+    
+    j ,k = int(0), int(0)
+    
+    print("lefthalft:",leftHalf)
+    print("righthalf:",rightHalf)
+
+    while i < len(array):
+        if j < len(leftHalf) and k < len(rightHalf):
+            if leftHalf[j] < rightHalf[k]:
+                array[i] = leftHalf[j]
+                leftHalf.pop(j)
+                i += 1
+            else:
+                array[i] = rightHalf[k]
+                rightHalf.pop(k)
+                i += 1
         else:
-            array[i] = rightHalf[k]
-            k = k + 1
+            if len(leftHalf) > 0:
+                array[i] = leftHalf[j]
+                leftHalf.pop(j)
+                i += 1
+            elif len(rightHalf) > 0:
+                array[i] = rightHalf[k]
+                rightHalf.pop(k)
+                i += 1
+            else:
+                return
+            print("array:", array)
 #this is a divide and conquer approach to sorting problem
 
 if __name__ == "__main__":
     first_array = [5, 3, 9, 2, 7]
+    i = int(0)
     mergeSort(first_array, 0, 4)
     print(first_array)
